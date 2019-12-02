a3173bca5f06b4cba88e750b4dc0e94c693000ff
p.vanchhay <p.vanchhay@inside.tworks.co.jp>
2019-12-02 10:50:58 +0900

    adf

--- hello/hello.go
+++ hello/hello.go
@@ -7,8 +7,12 @@ import (
 )
 
 func main() {
+<<<<<<< HEAD
 
 	fmt.Println(stringutil.Reverse("!retsaM"))
 	fmt.Println(stringutil.Reverse("!oG ,buhtiG opeR etavirP"))
 
+=======
+	fmt.Println(stringutil.Reverse("!oG ,buhtiG opeR etavirP test1 asdfasdf"))
+>>>>>>> adf
 }
